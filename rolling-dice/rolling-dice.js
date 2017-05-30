41a0241bbc5a5ec0a452101f44221460ce52e117
kamir222 <katia.amir@gmail.com>
2017-05-30 16:06:37 +0000

    added a function that return random number b/w 1-6

--- rolling-dice/rolling-dice.js
+++ rolling-dice/rolling-dice.js
@@ -4,8 +4,18 @@
 
 
 //take one parameter from command line (process.argv)
-//generate a six sided dice - a range from 1 - 6
-//generate a random number (Math.random)
+var parameter = process.argv[2];
+//generate a random number within the range of 1 - 6(Math.random)
+function randomDice () {
+	return Math.trunc(Math.random() * 6 + 1)
+}
+
+console.log(randomDice());
+
+//do the above operation parameter number of times 
+
+//parameter * randomDice()
+
 //print to the console "Rolled n dice: ",  ....
 
 
