23717e8b4c424613bf9b60d6db9ca444dad38da9
Katia <katia.amir@gmail.com>
2017-05-30 16:27:10 -0400

    fixed the pig laten code. Followed debugging steps

---
+++ debugging-errors/fixing-buggy-code-1.js
@@ -0,0 +1,15 @@
+var originalWords = process.argv.slice(2);
+
+var pigLatinWords = [];
+
+function translateToPigLatin(word) {
+  return word.slice(1, word.length) + word[0] + "ay";
+}
+
+for (var i = 0; i < originalWords.length; i++) {
+  pigLatinWords.push(translateToPigLatin(originalWords[i]));
+}
+
+console.log(pigLatinWords.join(' '));
+
+
