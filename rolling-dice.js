e108d7aa9a267b3ecf7185e0ea6477da95b4eb41
kamir222 <katia.amir@gmail.com>
2017-05-30 15:46:09 +0000

    uploading new file with pseudo-code

---
+++ rolling-dice.js
@@ -0,0 +1,11 @@
+//Write a program that takes a single parameter 
+//from the command line, a number, 
+//and rolls that many six-sided dice. 
+
+
+//take one parameter from command line (process.argv)
+//generate a six sided dice - a range from 1 - 6
+//generate a random number (Math.random)
+//print to the console "Rolled n dice: ",  ....
+
+
