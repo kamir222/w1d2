5d87f5345d8cf08e0169ee2016365ca011175dbd
kamir222 <katia.amir@gmail.com>
2017-05-30 17:13:19 +0000

    used arrays in my solution; works either way, but this way is more readable and shorter

--- rolling-dice/rolling-dice.js
+++ rolling-dice/rolling-dice.js
@@ -1,28 +1,17 @@
-//Write a program that takes a single parameter 
-//from the command line, a number, 
-//and rolls that many six-sided dice. 
-
-
-//take one parameter from command line (process.argv)
 var parameter = process.argv[2];
+
 //generate a random number within the range of 1 - 6(Math.random)
 function randomDice () {
 	return Math.trunc(Math.random() * 6 + 1);
 }
 
-//do the above operation parameter number of times 
+//call randomDice() a p number of times 
 function numberOfTimes (p) {
-	var input = 'Rolled ' + p + ' dice: ';
-	var input2 = randomDice();
+	var input2 = [];
 	for (var i = 1; i < p; i ++) {
-	  input2 += ', ' + randomDice();
+	  input2.push(randomDice());
 	}
-	return input + input2;
+	return 'Rolled ' + p + ' dice: ' + input2;
 }
 
 console.log(numberOfTimes(parameter));
-
-
-//print to the console "Rolled n dice: ",  ....
-
-
