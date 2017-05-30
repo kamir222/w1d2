c2edd571d80bef5c6cddeba9c34c8323d354567e
kamir222 <katia.amir@gmail.com>
2017-05-30 16:15:07 +0000

    loop working for number of dice to roll

--- rolling-dice/rolling-dice.js
+++ rolling-dice/rolling-dice.js
@@ -7,14 +7,17 @@
 var parameter = process.argv[2];
 //generate a random number within the range of 1 - 6(Math.random)
 function randomDice () {
-	return Math.trunc(Math.random() * 6 + 1)
+	return Math.trunc(Math.random() * 6 + 1);
 }
-
-console.log(randomDice());
 
 //do the above operation parameter number of times 
 
-//parameter * randomDice()
+
+for (var i = 0; i < parameter; i ++) {
+	console.log(i + ' ' + randomDice() + '\n');
+}
+
+
 
 //print to the console "Rolled n dice: ",  ....
 
